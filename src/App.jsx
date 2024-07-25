8fabbea9f1625373027df34060a0814a4e3e6d48
Arman Rasouli <paplix.dev@gmail.com>
2024-07-25 15:45:48 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+import CalendarComponent from './CalendarComponents';
+
+function App() {
+  return (
+    <div>
+      <CalendarComponent />
+    </div>
+  );
+}
+
+export default App;
