075bd6d568c1bb0a6ac725ce1817a2f9209da539
Arman Rasouli <paplix.dev@gmail.com>
2024-07-31 12:59:47 +0800

    Changing the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import CalendarComponent from './CalendarComponents';
 import Picker from './DatePicker';
 
-
-function App() {
+function MainApp() {
+  const navigate = useNavigate();
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/embed-calendar" element={<CalendarComponent />} />
-        <Route path="/embed-picker" element={<Picker />} />
-        <Route path="/" element={
-          <div>
-            <h1>Main App</h1>
-            <div>
-              <a href='/embed-calendar'><button>Calendar</button></a>
-              <a href='/embed-picker'><button>Picker</button></a>
-            </div>
-          </div>
-        } />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Main App</h1>
+      <div>
+        <button onClick={() => navigate('/embed-calendar')}>Calendar</button>
+        <button onClick={() => navigate('/embed-picker')}>Picker</button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/embed-calendar" element={<CalendarComponent />} />
+      <Route path="/embed-picker" element={<Picker />} />
+      <Route path="/" element={<MainApp />} />
+    </Routes>
   );
 }
 
